c5ce14817ce5c569778838e7f42535bc6ec5dca5
uvens <uventus1992@gmail.com>
2023-09-28 17:27:03 +0300

    add auto_tracking

--- scripts/flat_export_nw.py
+++ scripts/flat_export_nw.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 from datetime import datetime
+from parsed import ParsedDf
 
 headers_eng: dict = {
     "Год": "year",
@@ -82,6 +83,8 @@ class ExportNW(object):
         self.change_type_and_values(df)
         df = df.replace({np.nan: None, "NaT": None})
         df["direction"] = df["direction"].replace({"импорт": "import", "экспорт": "export", "каботаж": "cabotage"})
+        ParsedDf(df).get_port()
+        df = df.replace({np.nan: None, "NaT": None})
         self.write_to_json(df.to_dict('records'))
 
 
